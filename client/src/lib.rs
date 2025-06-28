c59976b4f014cc4d35afbee0f6583a2cb9052279
Thomas Axelsson <thomasa88@gmail.com>
2025-06-28 20:04:11 +0200

    Make Subscription available to clients

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,4 +1,6 @@
 pub mod jsonrpc;
 mod transports;
 
-pub use jsonrpsee::core::client::async_client::Client;
+pub use jsonrpsee::core::client::{async_client::Client};
+use jsonrpsee::core::client::Subscription as S;
+pub type Subscription = S<serde_json::Value>;
